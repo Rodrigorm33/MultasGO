3314a1fc33f30055a17b6e76bb1e8f444e85e494
Rodrigom33 <seu.email@exemplo.com>
2025-03-03 16:17:53 -0300

    Usa porta fixa 8080 no script start.sh

--- start.sh
+++ start.sh
@@ -205,5 +205,5 @@ python -m app.scripts.create_tables || echo "Erro ao criar tabelas, mas a aplica
 
 # Iniciar a aplicação completa
 echo "Iniciando a aplicação completa com uvicorn..."
-echo "Usando porta: $PORT"
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT --log-level debug 
+echo "Usando porta fixa: 8080"
+exec uvicorn app.main:app --host 0.0.0.0 --port 8080 --log-level debug 
